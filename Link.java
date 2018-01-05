213e1f880550d6e24b3e982cb80aee7e9074096d
TomCPE23 <31649132+TomCPE23@users.noreply.github.com>
2018-01-05 11:48:47 +0700

    Add files via upload

---
+++ Link.java
@@ -0,0 +1,17 @@
+package LinkList;
+
+public class Link {
+	 public long data;
+	  public Link next;
+
+	  public Link(long val) {
+	    data = val;
+	    next = null;
+	  }
+
+	  public void displayLink() {
+	    System.out.print(data + " ");
+	  }
+
+	}  // end class
+
